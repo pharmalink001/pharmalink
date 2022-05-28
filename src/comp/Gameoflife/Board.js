ac3471db3f9f51de72507a2a3d5735c76d4694f8
lawsondaniel <lawblaze4@gmail.com>
2022-05-28 23:26:49 +0100

    fixed the board state

--- src/comp/Gameoflife/Board.js
+++ src/comp/Gameoflife/Board.js
@@ -1,15 +1,31 @@
 import React from 'react'
 import Grid from './Grid'
 import { useState } from 'react';
+import produce from 'immer'
 
 const Board = () => {
     //setting states
     
     const [row, setrow] = useState(50);
     const [col, setcol] = useState(50)
+    const [isRunning, setisRunning] = useState(false)
+    const createGrid =()=>{
+        const arr = new Array()
+        for(let i=0;i<=row;i++){
+           arr.push(Array.from(Array(col), ()=> 0))
+        }
+        return arr
+    }
+    
+
+    //setting the value gotten from the arr into a sate
+    const [grid, setgrid] = useState(()=> createGrid())
   return (
     <div>
         <div>
+        <button onClick={()=>{
+                setisRunning(!isRunning)
+            }}>{!isRunning ? "start" : "stop"}</button>
             <button onClick={()=>{
                 setrow(50)
                 setcol(50)
@@ -26,10 +42,10 @@ const Board = () => {
                 setrow(20)
                 setcol(20)
             }}>20X20</button>
-            
+           
         </div>
-        <div style={{display:'center',justifyContent:'center',alignItems:'center'}}>
-        <Grid row={row} col={col}/>
+        <div style={{display:'flex',justifyContent:'center',alignItems:'center'}}>
+        <Grid isRunning={isRunning} row={row} col={col} grid={grid}setgrid={setgrid}/>
         </div>
         
     </div>
--- src/comp/Gameoflife/Grid.js
+++ src/comp/Gameoflife/Grid.js
@@ -1,56 +1,54 @@
 import { toBeChecked } from '@testing-library/jest-dom/dist/matchers'
 import React from 'react'
 import { useState } from 'react'
+import produce from 'immer'
+const Grid = ({col,grid,setgrid}) => {
 
-const Grid = ({row,col}) => {
- const [active, setactive] = useState(false)
- const [first, setfirst] = useState('')
- const [second, setsecond] = useState('')
-    let arr =[]
-    const call =(val)=>{
-        // setfirst(val.i)
-        // setsecond(val.j)
-    }
-    for(let i =0;i<row;i++){
-        
-        arr[i]=[]
-        for(let j= 0;j<col;j++){
-            arr[i][j]=0;
-            // console.log(`${first} and${second}`)
-          call({i,j})
-        }
-       
-    }
+ 
    
-    //setting the value gotten from the arr into a sate
-    const [grid, setgrid] = useState(arr)
-    const [color, setcolor] = useState(`#FFF`)
+   
+    // console.log(grid)
     //styles for grid
     const gridTemplate ={
         display:'grid',
-        height:'500px',
-        gridTemplateColumns:'repeat(50, 1fr)',
-        gridtemplaterows:' repeat(50, 1fr)'
+        gridTemplateColumns:`repeat(${col},20px)`,
+      
     }
     //styles for box
     const box ={
         width:'20px',
         height:'20px',
         border:'1px solid #000',
-        background:`${active?"black":"white"}`
+       
     }
+
   return (
     <>
        
-        <div style={gridTemplate}>
+        <div   style={gridTemplate}>
            {
                grid.map((col,x)=>{
                    return col.map((row,w)=>{
-                     return  <div onClick={
+                     return  <div key={ `${w}--${x}`} onClick={
                          ()=>{
-                         
+                             if(grid[x][w] == 0){
+                                const newGrid = produce(grid, gridCopy =>{
+                                    gridCopy[x][w]=1
+                                    
+                                 })
+                                 setgrid(newGrid)
+                             }else{
+                                const newGrid = produce(grid, gridCopy =>{
+                                    gridCopy[x][w]=0
+                                    
+                                 })
+                                 setgrid(newGrid)
+                             }
+                          
                          }
-                     } style={box}>{first + second}</div>
+                     } style={{...box, background:`${grid[x][w] ? "black":"white"}`}}>{
+                       
+                     }</div>
                    })
                })
            }
