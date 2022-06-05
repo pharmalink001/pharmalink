52054d7affef5902238cde57d6b84f87c22fd031
lawsondaniel <lawblaze4@gmail.com>
2022-06-06 00:35:55 +0100

    worked on the 404 page

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import Nav from './comp/landing page/Nav'
-
+import notfound from './not.gif'
 const NotFound = () => {
   return (
     <>
+    <div className='notfound'>
     <Nav/>
-    <div>NotFound</div>
+    </div>
+   
+    <div id="main">
+  <div className="fof">
+  <div>
+      <img src={notfound}/>
+    </div>
+    <h1>Page Not Found</h1>
+  </div>
+</div>
+
     </>
   )
 }
